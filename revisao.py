6b61b1fb6ebba097944daa05853acc2548a8bef3
unknown <giovana.m.souza@aluno.senai.br>
2024-12-02 11:09:05 -0500

    meu primeiro comando

--- revisao.py
+++ revisao.py
@@ -9,4 +9,26 @@ faltas = int(input())
 
 #caculo da média
 media = (nota1+nota2+nota3+nota4)/4
-print( media)
+
+#logica da situaçao
+if faltas > 31:
+    situacao = "Reprovado por faltas"
+elif media >= 8 :
+    situacao = "aprovado"
+elif media >= 5: #recuperação
+      recuperacao= float(input())#ler a nota da prova de recuperaçaõ
+      if recuperacao >= (10-media):
+      
+      else:
+
+      situacao = "reprovado na recuperaçõ"
+else:
+      situacao = "reprovado por media"
+
+#relatorio
+print("nome", nome)
+print("notas", nota1,nota2,nota3,nota4)
+print("faltas", faltas)
+print("média",media)
+print("situação",situacao)
+
