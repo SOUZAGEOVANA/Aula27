be6e2cb403101f3f2c865c78aeed1110970c6711
unknown <giovana.m.souza@aluno.senai.br>
2024-12-02 09:36:55 -0500

    um novo comando

---
+++ revisao.py
@@ -0,0 +1,12 @@
+#ler entrada de usuário
+nome = input() #ARMAZENAR o nome do aluno
+nota1 = float(input())# 4 notas do aluno
+nota2 = float(input())# 4 notas do aluno
+nota3 = float(input())# 4 notas do aluno
+nota4 = float(input())# 4 notas do aluno
+
+faltas = int(input())
+
+#caculo da média
+media = (nota1+nota2+nota3+nota4)/4
+print( media)
