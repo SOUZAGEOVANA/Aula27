7704f128b98a0ae93c18f19df5010847a51a96c3
giovana <giovana.m.souza@aluno.senai.br>
2024-12-03 09:39:51 -0500

    concluido

--- revisao.py
+++ revisao.py
@@ -1,34 +1,36 @@
 #ler entrada de usuário
-nome = input() #ARMAZENAR o nome do aluno
-nota1 = float(input())# 4 notas do aluno
-nota2 = float(input())# 4 notas do aluno
-nota3 = float(input())# 4 notas do aluno
-nota4 = float(input())# 4 notas do aluno
+cont = 0 #variavel que controla a repetição
+escolha_usuario = int(input()) #variavel que guarda quantas vezes o codigo vai rodar 
+while cont < escolha_usuario:
+    nome = input() #ARMAZENAR o nome do aluno
+    nota1 = float(input())# 4 notas do aluno
+    nota2 = float(input())# 4 notas do aluno
+    nota3 = float(input())# 4 notas do aluno
+    nota4 = float(input())# 4 notas do aluno
+    faltas = int(input())
 
-faltas = int(input())
+    #caculo da média
+    media = (nota1+nota2+nota3+nota4)/4
 
-#caculo da média
-media = (nota1+nota2+nota3+nota4)/4
+    #logica da situaçao
+    if faltas > 31:
+        situacao = "Reprovado por faltas"
+    elif media >= 8 :
+        situacao = "aprovado"
+    elif media >= 5: #recuperação
+        recuperacao= float(input())#ler a nota da prova de recuperaçaõ
+        if recuperacao >= (10-media):
+            situacao = "reprovado na recuperaçõ"
+        else:
+            situacao = "reprovado por media"
 
-#logica da situaçao
-if faltas > 31:
-    situacao = "Reprovado por faltas"
-elif media >= 8 :
-    situacao = "aprovado"
-elif media >= 5: #recuperação
-      recuperacao= float(input())#ler a nota da prova de recuperaçaõ
-      if recuperacao >= (10-media):
-      
-      else:
+    #relatorio
+    print("nome", nome)
+    print("notas", nota1,nota2,nota3,nota4)
+    print("faltas", faltas)
+    print("média",media)
+    print("situação",situacao)
+    cont = cont + 1
 
-      situacao = "reprovado na recuperaçõ"
-else:
-      situacao = "reprovado por media"
 
-#relatorio
-print("nome", nome)
-print("notas", nota1,nota2,nota3,nota4)
-print("faltas", faltas)
-print("média",media)
-print("situação",situacao)
 
