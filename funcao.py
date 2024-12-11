882d5c9013c2ad29cf0180f8a18635e0dfc1a28d
giovana <giovana.m.souza@aluno.senai.br>
2024-12-11 10:00:14 -0500

    fiz outro comando

--- funcao.py
+++ funcao.py
@@ -1,8 +1,16 @@
-#Declarar uma função
-def bom_dia():
-    print("BOM DIA!")
+#Declarar uma funçãO
+def saudacoes(hora_do_dia): #exebir a saudação correspondente a hora do dia
+     #condição para  DAR BOM DIA
+    if (hora_do_dia >= 0) and (hora_do_dia <= 12):
+         print("BOM DIA!!!")
+    #condição para DAR BOA TARDE     
+    elif  (hora_do_dia) >= 13 and (hora_do_dia) <= 18:
+         print("Boa tarde!")
+    else:
+         print("boa noite")     
+#fora da função
+#peço para usuário dizer a hora atual 
+resposta = int(input("digite que horas são"))
+#chamo a função passando para ela o paramento obrigatorio
 
-bom_dia()
-def BOA_NOITE():
-    print("boa_noite!")
-BOA_NOITE()
+saudacoes(resposta)
